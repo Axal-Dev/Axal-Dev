377aef585b89ab5d33daaa719a3617d64f48ad99
Axal-Dev <axaldevscript@gmail.com>
2024-08-29 18:30:08 +0200

    Luncher.lua

---
+++ Luncher.lua
@@ -0,0 +1,22 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+local GameId = game.GameId
+
+local Games = {
+    { name = "Phantom Forces", gameid = 113491250, link = "https://raw.githubusercontent.com/Axal-Dev/Axal-Script/main/Axal-Beta?token=GHSAT0AAAAAACWYIABF6Q6OB35RBIZLWY5QZWQRAZA"},
+}
+
+function Fetch(URL)
+    return game:HttpGet(tostring(URL))
+end
+
+
+for _, Supported in Games do
+    if Supported.gameid == GameId then
+        Library:Notify(`Axal-Dev has detected you being in {Supported.name}.`, 5)
+
+        loadstring(Fetch(Supported.link))()
+        return "loaded regular"
+    end
+end
+
+Library:Notify("Your game is not supported !")
