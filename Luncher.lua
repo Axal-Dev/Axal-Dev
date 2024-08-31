57553fdaef0dcbb5ac254021d4f7c5342088cbcf
Axal-Dev <axaldevscript@gmail.com>
2024-08-31 19:13:29 +0200

    Luncher.lua

--- Luncher.lua
+++ Luncher.lua
@@ -3,6 +3,7 @@ local GameId = game.GameId
 
 local Games = {
     { name = "Phantom Forces", gameid = 113491250, link = "https://raw.githubusercontent.com/Axal-Dev/Axal-Dev/main/Game/Phantom%20Forces.lua"},
+    { name = "test place", gameid = 299659045, link = "https://raw.githubusercontent.com/Axal-Dev/Axal-Dev/main/Game/Phantom%20Forces.lua"},
 }
 
 function Fetch(URL)
