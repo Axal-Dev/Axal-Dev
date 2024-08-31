8f601fc77a0f102068415d9e56deef90005563d1
Axal-Dev <axaldevscript@gmail.com>
2024-08-31 10:31:10 +0200

    Luncher2.0.lua

--- Luncher2.0.lua
+++ Luncher2.0.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Axal-Dev/Axal-Dev/main/Script/Axal-Esp.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
